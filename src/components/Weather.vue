99c7d3b75b7ae7e0a7987a20b786dce2461cbf24
Remi <remi@hpg2.lan>
2017-04-16 21:57:26 +0200

    Init

---
+++ src/components/Weather.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <h1>ColorWeather</h1>
+    <h2>Choose your city</h2>
+    <div class="card">
+      <div class="section double-padded"><p>New York</p></div>
+      <div class="section dark"><p>Chicago</p></div>
+      <div class="section darker"><p>Atlanta</p></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'weather',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
